9ffb96115de9d22388a3ecc91cceecfce1402b7a
thaheershaik92 <128355075+thaheershaik92@users.noreply.github.com>
2024-02-27 21:50:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,11 +34,3 @@ resource "aws_dynamodb_table" "terraform_lock" {
     name = "LockId"
     type = "S"
   }
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "Terraform_Demo"
-  }
-}
